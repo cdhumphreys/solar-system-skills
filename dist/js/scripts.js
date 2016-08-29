c279cdac77e57f5f8d9adcf54b16f6e10bfcb780
Carl Humphreys <humphreys.carl.d@gmail.com>
2016-08-29 12:50:59 +0100

    added small bobbing to planets and moons, added rotation to rocket towards mouse click

--- dist/js/scripts.js
+++ dist/js/scripts.js
@@ -1,1 +1,32 @@
 'use strict';
+
+var rocket = document.getElementById('rocket');
+var container = document.getElementById('container');
+
+var initialRocketProps = rocket.getBoundingClientRect();
+var initialRocketMidX = initialRocketProps.left + initialRocketProps.width / 2;
+var initialRocketMidY = initialRocketProps.top + initialRocketProps.height / 2;
+
+container.addEventListener('click', clickHandler);
+
+var currentAngle = 0;
+function clickHandler(event) {
+
+  var clickX = event.pageX;
+  var clickY = event.pageY;
+
+  var angle = getAngle(initialRocketMidX, initialRocketMidY, clickX, clickY);
+
+  // rocket.style.transform = 'translate(' + (clickX-initialRocketMidX) + 'px,' + (clickY-initialRocketMidY) +'px) rotate(' + currentAngle + 'deg)';
+  rocket.style.transform = 'rotate(' + angle + 'deg)';
+  // rocket.style.transform = 'translate(' + 0 + 'px,' + 0 +'px)';
+}
+
+function getAngle(x1, y1, x2, y2) {
+  var dx = x2 - x1;
+  var dy = y2 - y1;
+
+  // let theta = Math.atan2(dx, dy);
+  var theta = Math.atan2(dy, dx);
+  return 180 / Math.PI * theta + 90;
+}
--- src/js/scripts.js
+++ src/js/scripts.js
@@ -1,1 +1,32 @@
 'use strict';
+
+const rocket = document.getElementById('rocket');
+const container = document.getElementById('container');
+
+const initialRocketProps = rocket.getBoundingClientRect();
+const initialRocketMidX = initialRocketProps.left + (initialRocketProps.width/2);
+const initialRocketMidY = initialRocketProps.top + (initialRocketProps.height/2);
+
+container.addEventListener('click', clickHandler);
+
+let currentAngle = 0;
+function clickHandler(event) {
+
+  let clickX = event.pageX;
+  let clickY = event.pageY;
+
+  const angle = getAngle(initialRocketMidX, initialRocketMidY, clickX, clickY);
+
+  // rocket.style.transform = 'translate(' + (clickX-initialRocketMidX) + 'px,' + (clickY-initialRocketMidY) +'px) rotate(' + currentAngle + 'deg)';
+  rocket.style.transform = 'rotate(' + angle + 'deg)';
+  // rocket.style.transform = 'translate(' + 0 + 'px,' + 0 +'px)';
+}
+
+function getAngle(x1,y1, x2,y2) {
+  const dx = x2 - x1;
+  const dy = y2 - y1;
+
+  // let theta = Math.atan2(dx, dy);
+  const theta = Math.atan2(dy, dx);
+  return ((180/Math.PI) * theta)+90;
+}
